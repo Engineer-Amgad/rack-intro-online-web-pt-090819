74503572c430181daa3ad76022802ff96b591dfd
Amgad Malik <amgadmalik@yahoo.com>
2019-12-24 21:54:22 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -5,9 +5,5 @@ class Application
     resp.write "Hello, my name is Amgad"
     resp.finish
   end
-
-  def last_response
-    [200, { 'Content-Type' => 'text/html' }, ['<em>Hello, my name is Amgad</em>']]
-  end
 end
 
